9f4c9ad4911d8c2c351d55d8d6d7842f44c27ad9
FaizanSiddiquiii <buisnessoffaizan2003@gmail.com>
2025-07-06 16:54:13 +0500

    changed navbar text again

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -45,7 +45,7 @@ const Navigation = () => {
                 ? 'bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text text-transparent' 
                 : 'text-white'
             }`}>
-              SynergyX Services
+              SynergyX
             </span>
           </Link>
 
